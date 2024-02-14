941fc99f0e8ef1d7285e341b84cc5fb2c55e667a
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-02-14 08:53:23 +0200

    working

--- src/Components/GradientGenerator/CopyCSS.tsx
+++ src/Components/GradientGenerator/CopyCSS.tsx
@@ -1,22 +1,31 @@
-import React, { FC } from "react";
+import { FC, useState } from "react";
 
 type CopyCSSProps = {
   cssCode: string;
 };
 
 const CopyCSS: FC<CopyCSSProps> = ({ cssCode }) => {
+  const [copied, setCopied] = useState(false);
+
   const copyToClipboard = () => {
     navigator.clipboard
       .writeText(cssCode)
-      .then(() => alert("Copied to clipboard!"))
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+      })
       .catch((err) => console.error("Failed to copy text: ", err));
   };
 
   return (
     <div className="d-flex justify-content-center align-items-center">
-      <span id="text">{cssCode} </span>
-      <button id="copy" onClick={copyToClipboard}>
-        Copy
+      <span id="text">{cssCode}</span>
+      <button
+        id="copy"
+        onClick={copyToClipboard}
+        className={copied ? "copied" : ""}
+      >
+        {copied ? "Copied!" : "Copy"}
       </button>
     </div>
   );
